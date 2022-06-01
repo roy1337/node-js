1e082572a9269d2a670dc99435d32d5b780ad458
Roy <40725093+roy1337@users.noreply.github.com>
2022-06-01 10:12:41 +0300

    Update raceParSeqPromises.js

--- raceParSeqPromises.js
+++ raceParSeqPromises.js
@@ -8,12 +8,14 @@ const a = () => promisify('a', 100);
 const b = () => promisify('b', 5000);
 const c = () => promisify('c', 3000);
 
+//parallel run, output are all, in order of finish time - done parallely.
 async function parallelFunc() {
   const arrayOfPromises = [a(),b(),c()];
   const [outpu1,output2,output3] = Promise.all(arrayOfPromises)
   return `parallel is done: ${output1} ${output2} ${output3}`
 }
 
+//race run, output is the fastest.
 async function raceFunc() {
   const arrayOfPromises = [a(),b(),c()]
   const output1 = Promise.race(arrayOfPromises)
@@ -21,6 +23,7 @@ async function raceFunc() {
 
 }
 
+//sequence run, output are all - done one by one.
 async function sequnceFunc() {
   const outpu1 = await a()
   const output2 = await b()
@@ -28,6 +31,9 @@ async function sequnceFunc() {
   return `sequence is done ${output1} ${output2} ${output3}`
 
 }
+
+//making a race between the three options, ofcourse Race will be logged first,
+//second is parallel and third is the sequence function.
 
 sequenceFunc().then(console.log)
 parallelFunc().then(console.log)
